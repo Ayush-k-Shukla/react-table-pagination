17d1c7c6d5f3503f96bdacc4f02a0b3e4de96f2d
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2023-02-02 21:03:49 +0530

    some dependecy removed

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 import { QueryClient, QueryClientProvider } from 'react-query';
 const queryClient = new QueryClient();
